8c09aa383cd6ffb0d98d298f758529d40b9154aa
Song Tan <song.tan@y7mail.com>
2012-07-08 11:25:00 +1000

    starting development
    Signed-off-by: Song Tan <song.tan@y7mail.com>

--- lib/acts_as_customizable.rb
+++ lib/acts_as_customizable.rb
@@ -7,57 +7,10 @@ module ActsAsCustomizable
 
   require "digest/sha1"
 
-  $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-  module ActsAsTaggableOn
-    mattr_accessor :delimiter
-    @@delimiter = ','
-
-    mattr_accessor :force_lowercase
-    @@force_lowercase = false
-
-    mattr_accessor :force_parameterize
-    @@force_parameterize = false
-
-    mattr_accessor :remove_unused_tags
-    self.remove_unused_tags = false
-
-    def self.glue
-      delimiter = @@delimiter.kind_of?(Array) ? @@delimiter[0] : @@delimiter
-      delimiter.ends_with?(" ") ? delimiter : "#{delimiter} "
+  class ActsAsCustomizable
+    def self.hi
+      puts "Hello world!"
     end
-
-    def self.setup
-      yield self
-    end
-  end
-
-
-  require "acts_as_taggable_on/utils"
-
-  require "acts_as_taggable_on/taggable"
-  require "acts_as_taggable_on/acts_as_taggable_on/core"
-  require "acts_as_taggable_on/acts_as_taggable_on/collection"
-  require "acts_as_taggable_on/acts_as_taggable_on/cache"
-  require "acts_as_taggable_on/acts_as_taggable_on/ownership"
-  require "acts_as_taggable_on/acts_as_taggable_on/related"
-  require "acts_as_taggable_on/acts_as_taggable_on/dirty"
-
-  require "acts_as_taggable_on/tagger"
-  require "acts_as_taggable_on/tag"
-  require "acts_as_taggable_on/tag_list"
-  require "acts_as_taggable_on/tags_helper"
-  require "acts_as_taggable_on/tagging"
-
-  $LOAD_PATH.shift
-
-
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.extend ActsAsTaggableOn::Taggable
-    ActiveRecord::Base.send :include, ActsAsTaggableOn::Tagger
-  end
-
-  if defined?(ActionView::Base)
-    ActionView::Base.send :include, ActsAsTaggableOn::TagsHelper
   end
 end
